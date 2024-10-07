163edea9de33fe143526216d8d06367327661ac5
Thanaphon <dthanaphon21@gmail.com>
2024-10-07 19:24:47 +0700

    vercelWEGGH

--- webpage/wsgi.py
+++ webpage/wsgi.py
@@ -13,3 +13,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webpage.settings')
 
 application = get_wsgi_application()
+
+# เพิ่ม alias สำหรับ Vercel
+app = application
